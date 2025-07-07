879e684b87fb61b22ed4382bbdf4e0d1c2c45dd3
Prakhar Shukla <therockstarop9956@gmail.com>
2025-07-07 13:08:38 +0530

    Fabonacci Number

---
+++ DynamicProgramming/FabonacciNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FabonacciNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int result = fibonacci(n);
+        System.out.println("Fibonacci number at position " + n + " is: " + result);
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
